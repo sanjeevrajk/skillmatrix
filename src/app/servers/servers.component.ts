8cde93cbc683b5151b604a94d139cde40b2acf1e
sanjeevrajk <sanjeev.tvr@gmail.com>
2019-02-27 07:33:40 +0530

    Add files via upload

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `<app-server></app-server>hh<h1>ghdfb</h1>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
